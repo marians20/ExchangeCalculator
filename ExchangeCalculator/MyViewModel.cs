f0fe18c8197d7a3f9709b38d484cada7dec19818
Marian Spoiala <marian.spoiala@gmail.com>
2017-04-16 22:06:34 +0300

    Added solution

---
+++ ExchangeCalculator/MyViewModel.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExchangeCalculator
+{
+    public class MyViewModel : INotifyPropertyChanged
+    {
+        #region props
+        public List<Currency> Currencies { get; set; }
+        public string InputCurrencyId
+        {
+            get
+            {
+                return _inputCurrencyId;
+            }
+            set
+            {
+                if (string.CompareOrdinal(value, _inputCurrencyId) == 0) return;
+                _inputCurrencyId = value;
+                Compute();
+            }
+        }
+        private string _inputCurrencyId;
+
+        public double InputValue
+        {
+            get { return _inputValue; }
+            set
+            {
+                if (Math.Abs(value - _inputValue) < double.Epsilon) return;
+                _inputValue = value;
+                RaisePropertyChanged("InputValue");
+                Compute();
+            }
+        }
+        private double _inputValue;
+
+        public string OutputCurrencyId
+        {
+            get { return _outputConcurencyId; }
+            set
+            {
+                if (string.CompareOrdinal(value, _outputConcurencyId) == 0) return;
+                _outputConcurencyId = value;
+                RaisePropertyChanged("OutputCurrencyId");
+            }
+        }
+        private string _outputConcurencyId;
+
+        public double OutputValue
+        {
+            get
+            {
+                return _outputValue;
+            }
+            set
+            {
+                if (Math.Abs(value - _outputValue) < double.Epsilon) return;
+                _outputValue = value;
+                RaisePropertyChanged("OutputValue");
+            }
+        }
+        private double _outputValue;
+
+        public bool IsValidInput
+        {
+            get
+            {
+                return !double.IsNaN(InputValue) &&
+                Currencies.Exists(i => i.id == InputCurrencyId) &&
+                Currencies.Exists(i => i.id == OutputCurrencyId);
+            }
+        }
+        #endregion
+
+        #region ctor
+
+        public MyViewModel()
+        {
+            var currencies = Rates.GetBnrExchangeRates().ToList();
+            Currencies = new List<Currency>();
+            InputValue = 1.0;
+            Currencies.Add(new Currency() { id = Guid.NewGuid().ToString(), currency = "RON", rate = 1 });
+            Currencies.AddRange(currencies);
+            InputCurrencyId = Currencies.Exists(i => string.Compare(i.currency, "eur", StringComparison.OrdinalIgnoreCase) == 0)
+                ? Currencies.AsParallel().First(i => string.Compare(i.currency, "eur", StringComparison.OrdinalIgnoreCase) == 0).id
+                : Currencies.First().id;
+            OutputCurrencyId =
+                Currencies.AsParallel().First(i => string.Compare(i.currency, "ron", StringComparison.OrdinalIgnoreCase) == 0).id;
+            Compute();
+        }
+        #endregion
+
+        private void Compute()
+        {
+            if (!IsValidInput) return;
+            OutputValue = InputValue*Currencies.AsParallel().First(i => i.id == InputCurrencyId).rate/
+                            Currencies.AsParallel().First(i => i.id == OutputCurrencyId).rate;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void RaisePropertyChanged(string property)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(property));
+        }
+    }
+}
+
---
+++ ExchangeCalculator/Rates.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExchangeCalculator
+{
+    public class Currency
+    {
+        public string id { get; set; }
+        public string currency { get; set; }
+        public double rate { get; set; }
+
+        public override string ToString()
+        {
+            return $"{currency}\t{rate}";
+        }
+    }
+    public class Rates
+    {
+        public static IEnumerable<Currency> GetBnrExchangeRates()
+        {
+            var result = new List<Currency>();
+            const string url = "http://www.bnro.ro/nbrfxrates.xml";
+            var request = (HttpWebRequest)WebRequest.Create(url);
+            var body = string.Empty;
+            try
+            {
+                var dataSet = new DataSet();
+                if (!File.Exists("nbrfxrates.xsd"))
+                    Wget("http://www.bnr.ro/xsd/nbrfxrates.xsd", "nbrfxrates.xsd");
+                dataSet.ReadXmlSchema("nbrfxrates.xsd");
+                var response = request.GetResponseAsync().Result;
+                using (var receiveStream = response.GetResponseStream())
+                using (var readStream = new StreamReader(receiveStream, Encoding.UTF8))
+                    dataSet.ReadXml(readStream);
+                var data = dataSet.Tables["Rate"].AsEnumerable().ToList();
+                foreach (var i in data)
+                {
+                    var currency = i.Field<string>("Currency");
+                    var rate = (double)i.Field<decimal>("Rate_text");
+                    var multiplier = (i.Field<long?>("multiplier")) ?? 1;
+                    result.Add(new Currency()
+                    {
+                        id = Guid.NewGuid().ToString(),
+                        currency = currency,
+                        rate = rate / (multiplier != 0 ? multiplier : 1)
+                    });
+                };
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            return result;
+        }
+
+        public static async Task<bool> Wget(string url, string destinationFileName)
+        {
+            var request = (HttpWebRequest)WebRequest.Create(url);
+            var body = string.Empty;
+            try
+            {
+                var response = await request.GetResponseAsync();
+                using (var input = response.GetResponseStream())
+                using (var output = File.OpenWrite(destinationFileName))
+                {
+                    input?.CopyTo(output);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+                return false;
+            }
+            return true;
+        }
+    }
+}
